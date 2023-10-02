54ce50964b39b1eed26f7cbd9f15feb72d7134a8
biafranca <bianca.franca@bling.com.br>
2023-10-02 17:29:48 -0300

    feat: cria models

---
+++ src/models/Book.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { authorSchema } from "./Author.js";
+
+const  bookSchema = new mongoose.Schema({
+    id: {
+        type: mongoose.Schema.Types.ObjectId
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String
+    },
+    price: {
+        type: Number
+    },
+    pages: {
+        type: Number
+    },
+    author: authorSchema
+}, { versionKey: false });
+
+const book = mongoose.model('books', bookSchema);
+
+export default book;
